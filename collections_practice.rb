1394cb9976a56166d00a39168a19927b3a317073
Keanna Salter <keannasalter91@gmail.com>
2020-02-02 19:20:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,16 +8,10 @@ def sort_array_desc(array)
   end
 end
 
-# def sort_array_char_count(array)
-#   array.sort do |left, right|
-#     left.length <=> right.length
-#   end
-# end
-
-# def swap_elements(array)
-#   array[1], array[2] = array[2], array[1]
-#   array
-# end
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
 
 # def reverse_array(array)
 #   array.reverse
