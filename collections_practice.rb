89277dc50b6a83ca61a2c9a857a0345d840a844b
Keanna Salter <keannasalter91@gmail.com>
2020-02-02 19:17:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,11 +2,11 @@ def sort_array_asc(array)
   array.sort 
 end
 
-# def sort_array_asc(array)
-#   array.sort do |left, right|
-#     right <=> left
-#   end
-# end
+def sort_array_asc(array)
+  array.sort do |left, right|
+    right <=> left
+  end
+end
 
 # def sort_array_char_count(array)
 #   array.sort do |left, right|
